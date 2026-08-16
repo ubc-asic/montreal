53d70caaf3c1facab3daa3efda3135255392daa4
Chat <63841542+chatrajaman3@users.noreply.github.com>
2026-08-15 17:21:48 -0700

    Apply suggestions from code review
    Co-authored-by: Warrick Lo <warrick.s.z.lo@gmail.com>
    Signed-off-by: Chat <63841542+chatrajaman3@users.noreply.github.com>

--- flows/common.mk
+++ flows/common.mk
@@ -1,7 +1,7 @@
-TOP        		:= tt_top_ubc_montreal
+TOP        	        := tt_top_ubc_montreal
 OUTPUT_DIR  	:= output
 RTL_FILELIST 	:= rtl/filelist.f
-#DV_FILELIST  	:= dv/filelist.f
+DV_FILELIST  	:= dv/filelist.f
 
 VERILATOR  := verilator
 YOSYS      := yosys
